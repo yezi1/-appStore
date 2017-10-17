90d8cab2d80cee28a71075b0e1bc7dc7473026d2
yezi1 <13719490809@163.com>
2017-10-17 16:47:23 +0800

    首页完成

--- js/index.js
+++ js/index.js
@@ -1,4 +1,8 @@
 $(function () {
+	
+	/***
+	 * 头部菜单
+	 */
 	var menuStatus = true;
 	$('.catalogue').on('click',function () {
 		menuStatus = !menuStatus;
@@ -12,9 +16,22 @@ $(function () {
 		}
 	});
 	
-    $('.menu-content,.PopUp-layer').on('click',function () {
+    $('.menu-content,.PopUp-layer,.search').on('click',function () {
     	menuStatus = !menuStatus;
     	$('.header-menu').removeClass('header-menu-on');
 		$('.PopUp-layer').removeClass('PopUp-layer-on');
+    });
+    
+    /***
+     * 搜索框
+     */
+    $('.search').on('click',function () {
+    	$('.public-header-2').addClass('public-header-on');
+    	$('.search-layer').addClass('search-layer-on');
+    });
+    
+    $('.return-goto').on('click',function () {
+    	$('.public-header-2').removeClass('public-header-on');
+    	$('.search-layer').removeClass('search-layer-on');
     });
 });
